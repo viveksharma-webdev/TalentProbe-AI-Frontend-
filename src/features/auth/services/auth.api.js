c34ea12bec67cff88596084cc9ac0a2d88baca73
vivek <vivek45559s@gmailcom>
2026-03-25 19:42:11 +0530

    deploy

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://talentprobe-ai.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
 
